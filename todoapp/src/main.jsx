5a5a86ce179f36394ad0bc7cd70db8c3998b3c80
rachid aourik <rachidaourik18@gmail.com>
2025-12-17 23:09:01 +0100

    trying to fix it

--- todoapp/src/main.jsx
+++ todoapp/src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import EXo02 from './componment/changebackroundcolorandsize.jsx'
-import Text from './componment/input.jsx'
-import useee from './componment/carddd.jsx'
-import UpLower  from './componment/card01.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
