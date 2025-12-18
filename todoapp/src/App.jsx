8e4aff56f78758152652aa290516440b98df64c4
rachid aourik <rachidaourik18@gmail.com>
2025-12-18 23:53:36 +0100

    test.js

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 
-import useee  from './componment/carddd'
-import UpLower from './componment/card01'
 
 function App() {
 
   return (
     <>
      <div className='app'>
-      <UpLower />
+   
      </div>
       
     </>
