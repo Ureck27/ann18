e0f33eabc580aee466860bd59ff5dc2818bdfd86
rachid aourik <rachidaourik18@gmail.com>
2025-12-19 20:51:26 +0100

    start to creath componment homepage

---
+++ test_project_with_react/src/componment/Homepage.componment.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+const Homepage =() =>{
+    
+
+
+      return (
+        <>
+          <div className="container_Homepage">
+             <div className="drictory_menu">
+                <div className="menu-item">
+                  <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtatle">Shop Now</span>
+                  </div>
+                </div>
+                <div className="menu-item">
+                  <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtatle">Shop Now</span>
+                  </div>
+                </div>
+                <div className="menu-item">
+                  <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtatle">Shop Now</span>
+                  </div>
+                </div>
+                <div className="menu-item">
+                  <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtatle">Shop Now</span>
+                  </div>
+                </div>
+                <div className="menu-item">
+                  <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtatle">Shop Now</span>
+                  </div>
+                </div>
+             </div>
+          </div>
+        
+        </>
+      )
+}
+
+export default Homepage ;
